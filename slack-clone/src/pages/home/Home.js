279e443d8f435cd722ad3853f638049c5b09fb23
Adams Okode <adamsokode@gmail.com>
2021-12-27 17:17:53 +0300

    react router implementation

---
+++ slack-clone/src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "../../layouts";
+import "../../App.css";
+
+function Home() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
