abd86f23f7ca00678d2df8d0cc603e65036a408b
unknown <joyce.mungai@simpleformations.ke>
2021-12-27 15:31:09 +0300

    Configure firebase

---
+++ slack-clone/src/firebase.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCezqMlldR6dukdDjIYoDIgA_swujP0T3g",
+  authDomain: "slack-clone-5c723.firebaseapp.com",
+  projectId: "slack-clone-5c723",
+  storageBucket: "slack-clone-5c723.appspot.com",
+  messagingSenderId: "662549808112",
+  appId: "1:662549808112:web:17c45e08f8d8e138f29335",
+  measurementId: "G-2BVR7E3XWP"
+};
+
+export default firebaseConfig
